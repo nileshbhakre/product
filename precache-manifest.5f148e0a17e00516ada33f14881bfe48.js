8827101678d22907172b910363efde72f2b1067d
nileshbhakre <nileshbhakre11@gmail.com>
2020-02-19 14:32:52 +0530

    Updates

---
+++ precache-manifest.5f148e0a17e00516ada33f14881bfe48.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed8ec64a2e4c4a096c58c266b385aa99",
+    "url": "/product/index.html"
+  },
+  {
+    "revision": "dc55d24a49ccff69db04",
+    "url": "/product/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "79df1109fbf231686919",
+    "url": "/product/static/js/2.876ac51f.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/product/static/js/2.876ac51f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc55d24a49ccff69db04",
+    "url": "/product/static/js/main.cbacda24.chunk.js"
+  },
+  {
+    "revision": "a517a2d16b42cdf173ef",
+    "url": "/product/static/js/runtime-main.7bce59dc.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/product/static/media/logo.5d5d9eef.svg"
+  }
+]);
